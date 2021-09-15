961242800051269204ab076ffcee032a76af0a33
E-Wickham <ericwickham@hotmail.com>
2021-09-15 13:14:04 -0400

    update Sept 15 1:13pm

--- js/folder.js
+++ js/folder.js
@@ -1,70 +1,70 @@
 jQuery(document).ready(function () {
     
-    $('#coding').hover(
+    $('#web-projects').hover(
         function(){
             $(this).css("background-color", "pink");
             $(this).css("z-index","1");
             $('#folderBody').css("background-color", "pink");
             //set behind folder body
-            $('#podcasts').css("z-index","-1");
-            $('#journalism').css("z-index","-1");
+            $('#scripts').css("z-index","-1");
+            $('#web-design').css("z-index","-1");
             $('#misc').css("z-index","-1");
             //change the colour back to default
-            $('#podcasts').css("background-color", "#E3E0AF");
-            $('#journalism').css("background-color", "#E3E0AF");
+            $('#scripts').css("background-color", "#E3E0AF");
+            $('#web-design').css("background-color", "#E3E0AF");
             $('#misc').css("background-color", "#E3E0AF");
-            //show coding portfolio content and hide the others
-            $('#coding-folder-content').show();
-            $('#journalism-folder-content').hide();
-            $('#podcast-folder-content').hide();
+            //show web-projects portfolio content and hide the others
+            $('#web-projects-folder-content').show();
+            $('#web-design-folder-content').hide();
+            $('#script-folder-content').hide();
             $('#misc-folder-content').hide();
 
             }, function(){
-            $('#podcasts').css("z-index","1");
-            $('#journalism').css("z-index","1");
+            $('#scripts').css("z-index","1");
+            $('#web-design').css("z-index","1");
             $('#misc').css("z-index","1");
         }
     )
-    $('#podcasts').hover(
-        function(){
-            $(this).css("background-color", "skyblue");
-            $(this).css("z-index","1");
-            $('#folderBody').css("background-color", "skyblue");
-            $('#coding').css("z-index","-1");
-            $('#journalism').css("z-index","-1");
-            $('#misc').css("z-index","-1");
-            $('#coding').css("background-color", "#E3E0AF");
-            $('#journalism').css("background-color", "#E3E0AF");
-            $('#misc').css("background-color", "#E3E0AF");
-            //show coding portfolio content and hide the others
-            $('#podcast-folder-content').show();
-            $('#coding-folder-content').hide();
-            $('#journalism-folder-content').hide();
-            $('#misc-folder-content').hide();
-            }, function(){
-            $('#coding').css("z-index","1");
-            $('#journalism').css("z-index","1");
-            $('#misc').css("z-index","1");
-        }
-    )
-    $('#journalism').hover(
+    $('#scripts').hover(
         function(){
             $(this).css("background-color", "#A2E4B8");
+            $(this).css("z-index","1");
             $('#folderBody').css("background-color", "#A2E4B8");
-            $('#coding').css("z-index","-1");
-            $('#podcasts').css("z-index","-1");
+            $('#web-projects').css("z-index","-1");
+            $('#web-design').css("z-index","-1");
             $('#misc').css("z-index","-1");
-            $('#coding').css("background-color", "#E3E0AF");
-            $('#podcasts').css("background-color", "#E3E0AF");
+            $('#web-projects').css("background-color", "#E3E0AF");
+            $('#web-design').css("background-color", "#E3E0AF");
             $('#misc').css("background-color", "#E3E0AF");
-            $('#journalism-folder-content').show();
-            $('#coding-folder-content').hide();
-            $('#podcast-folder-content').hide();
+            //show web-projects portfolio content and hide the others
+            $('#script-folder-content').show();
+            $('#web-projects-folder-content').hide();
+            $('#web-design-folder-content').hide();
+            $('#misc-folder-content').hide();
+            }, function(){
+            $('#web-projects').css("z-index","1");
+            $('#web-design').css("z-index","1");
+            $('#misc').css("z-index","1");
+        }
+    )
+    $('#web-design').hover(
+        function(){
+            $(this).css("background-color", "skyblue");
+            $('#folderBody').css("background-color", "skyblue");
+            $('#web-projects').css("z-index","-1");
+            $('#scripts').css("z-index","-1");
+            $('#misc').css("z-index","-1");
+            $('#web-projects').css("background-color", "#E3E0AF");
+            $('#scripts').css("background-color", "#E3E0AF");
+            $('#misc').css("background-color", "#E3E0AF");
+            $('#web-design-folder-content').show();
+            $('#web-projects-folder-content').hide();
+            $('#script-folder-content').hide();
             $('#misc-folder-content').hide();
 
             }, function(){
-            $('#coding').css("z-index","1");
-            $('#podcasts').css("z-index","1");
+            $('#web-projects').css("z-index","1");
+            $('#scripts').css("z-index","1");
             $('#misc').css("z-index","1");
         }
     )
@@ -73,39 +73,39 @@ jQuery(document).ready(function () {
             $(this).css("background-color", "#FFDF9E");
             $('#folderBody').css("background-color", "#FFDF9E");
             //drop the 
-            $('#coding').css("z-index","-1");
-            $('#podcasts').css("z-index","-1");
-            $('#journalism').css("z-index","-1");
+            $('#web-projects').css("z-index","-1");
+            $('#scripts').css("z-index","-1");
+            $('#web-design').css("z-index","-1");
             //change the colour back to default
-            $('#coding').css("background-color", "#E3E0AF");
-            $('#podcasts').css("background-color", "#E3E0AF");
-            $('#journalism').css("background-color", "#E3E0AF");
+            $('#web-projects').css("background-color", "#E3E0AF");
+            $('#scripts').css("background-color", "#E3E0AF");
+            $('#web-design').css("background-color", "#E3E0AF");
             //show misc items and hide the rest
             $('#misc-folder-content').show();
-            $('#journalism-folder-content').hide();
-            $('#coding-folder-content').hide();
-            $('#podcast-folder-content').hide();
+            $('#web-design-folder-content').hide();
+            $('#web-projects-folder-content').hide();
+            $('#script-folder-content').hide();
             }, function(){
-            $('#coding').css("z-index","1");
-            $('#podcasts').css("z-index","1");
-            $('#journalism').css("z-index","1");
+            $('#web-projects').css("z-index","1");
+            $('#scripts').css("z-index","1");
+            $('#web-design').css("z-index","1");
         }
     )
 
 // MODAL
-$('#coding').click(
+$('#web-projects').click(
     function(){
-    $('#codingModal').modal('show');
+    $('#web-projectsModal').modal('show');
     });
 
-$('#podcasts').click(
+$('#scripts').click(
     function(){
-    $('#podcastModal').modal('show');
+    $('#scriptModal').modal('show');
     });
 
-$('#journalism').click(
+$('#web-design').click(
     function(){
-    $('#journoModal').modal('show');
+    $('#designModal').modal('show');
     });
 $('#misc').click(
     function(){
